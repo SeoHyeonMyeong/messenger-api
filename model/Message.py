c487310a5d01c6abe69aa7c8411f7af5fb6a8fb7
SeoHyeonMyeong <gusaud2199@naver.com>
2022-04-28 13:49:10 +0900

    Update: simplify Message Model by using pydentic library

--- model/Message.py
+++ model/Message.py
@@ -1,16 +1,6 @@
+from pydantic import BaseModel
 
-class Message():
-    
-    def __init__(self, dict):
-        self.title = dict["title"]
-        self.content = dict["content"]
-    
-    def get_content(self):
-        return self.content
-    
-    def get_data(self):
-        data = {
-            "title": self.title,
-            "content": self.content,
-        }
-        return data
+class Message(BaseModel):
+    title: str
+    content: str
+    validation: bool = True
