b1953ae70c0755286c1de4f21f29f2eb2ce2efa7
SeoHyeonMyeong <gusaud2199@naver.com>
2022-04-28 17:04:22 +0900

    Update: make connection to PostgreDB by using psycopg2 library

--- app/main.py
+++ app/main.py
@@ -1,69 +1,100 @@
 from fastapi import FastAPI, Response, status, HTTPException
 from model.Message import Message
-import itertools
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
-messages = dict()
-id_counter = itertools.count(1)
 
-m = Message(title="Hi",content="Hello").dict()
-id = next(id_counter)
-m['id'] = id
-messages[id] = m
-m = Message(title="Nice to meet you!", content="...").dict()
-id = next(id_counter)
-m['id'] = id
-messages[id] = m
-print(messages.values())
+# DB 연결
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',
+                                database='messenger', 
+                                user='postgres', 
+                                password='password',
+                                cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!")
+        break
+    except Exception as error:
+        print("Connecting to database Failed!")
+        print("Error: ", error)
+        time.sleep(2)
 
 # 우선순위는 위에 있는 함수!
+# [GET] Root
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-
+# [POST] Create Message
 @app.post("/messages", status_code=status.HTTP_201_CREATED)
 def create_message(message: Message):
-    m = message.dict()
-    id = next(id_counter)
-    m['id'] = id
-    messages[id] = m
-    return {"success": "True"}
+    
+    cursor.execute("""INSERT INTO messages (title, content) VALUES (%s,%s) RETURNING * """,
+                   (message.title, message.content))
+    message = cursor.fetchone()
+    conn.commit()
+    
+    return {"data": message}
 
-
+# [GET] List Messages
 @app.get("/messages")
 def list_messages():
-    return {"data": list(messages.values())}
+    
+    cursor.execute("""SELECT * FROM messages;""")
+    messages = cursor.fetchall()
+    
+    return {"data": messages}
 
-
+# [GET] Get Latest Message
 @app.get("/messages/latest")
 def get_latest_message():
-    m = list(messages.values())[-1]
-    return {"message": m}
+    
+    cursor.execute("""SELECT * FROM messages ORDER BY created_at DESC LIMIT 1""")
+    message = cursor.fetchone()
+    
+    return {"data": message}
 
-
+# [GET] Get Message
 @app.get("/messages/{id}")
-def get_message(id: int, response: Response):
-    if not id in messages:
+def get_message(id: int):
+    
+    cursor.execute("""SELECT * FROM messages WHERE id = %s""",(str(id)))
+    message = cursor.fetchone()
+    
+    if message == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"message with id: {id} was not found")
-    message = messages[id]
-    return {"message": message}
+        
+    return {"data": message}
 
-
+# [PUT] Update Message
 @app.put("/messages/{id}")
 def update_message(id: int, message: Message):
-    if not id in messages:
+    
+    cursor.execute("""UPDATE messages SET title = %s, content = %s WHERE id = %sRETURNING *""",
+                   (message.title, message.content, str(id),))
+    message = cursor.fetchone()
+    conn.commit()
+    
+    if message == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"message with id: {id} was not found")
-    messages[id] = message
-    return {"success": "True"}
 
+    return {"data": message}
 
+# [DELETE] Delete Message
 @app.delete("/messages/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_message(id: int):
-    if not id in messages:
+    
+    cursor.execute("""DELETE FROM messages WHERE id = %s RETURNING * """, (str(id),))
+    message = cursor.fetchone()
+    conn.commit()
+    
+    if message == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"message with id: {id} was not found")
-    del(messages[id])
+        
     return Response(status_code=status.HTTP_204_NO_CONTENT)
