2e2fcfa9f895119ba74e9e13648707cf4a3816fd
SeoHyeonMyeong <gusaud2199@naver.com>
2022-04-28 13:50:51 +0900

    Update: make new apis {delete-message, get-one-message, get-latest-message}

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
-from fastapi import Body, FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from model.Message import Message
+import itertools
 
 app = FastAPI()
-m = []
+messages = dict()
+id_counter = itertools.count(1)
+
+m = Message(title="Hi",content="Hello").dict()
+id = next(id_counter)
+m['id'] = id
+messages[id] = m
+m = Message(title="Nice to meet you!", content="...").dict()
+id = next(id_counter)
+m['id'] = id
+messages[id] = m
+print(messages.values())
+
 # 우선순위는 위에 있는 함수!
 @app.get("/")
 def root():
     return {"message": "Hello World"}
 
-@app.post("/send")
-def send_message(payload: dict = Body(...)):
-    m.append(Message(payload))
+
+@app.post("/messages", status_code=status.HTTP_201_CREATED)
+def send_message(message: Message):
+    m = message.dict()
+    id = next(id_counter)
+    m['id'] = id
+    messages[id] = m
     return {"success": "True"}
 
-@app.get("/list")
+
+@app.get("/messages")
 def list_message():
-    list = [item.get_data() for item in m]
-    return {"data": list}
+    return {"data": list(messages.values())}
+
+
+@app.get("/messages/latest")
+def get_latest_message():
+    m = list(messages.values())[-1]
+    return {"message": m}
+
+
+@app.get("/messages/{id}")
+def get_message(id: int, response: Response):
+    if not id in messages:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"message with id: {id} was not found")
+    message = messages[id]
+    return {"message": message}
+
+
+@app.delete("/messages/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_message(id: int):
+    if not id in messages:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"message with id: {id} was not found")
+    del(messages[id])
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
